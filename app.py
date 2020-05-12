9d8151c4ffaf4e9f5959aa9784fac9dc757d7d87
mosabami <ayobamiaayodeji@gmail.com>
2020-05-12 17:46:46 -0400

    added gunicorn to requirements

--- app.py
+++ app.py
@@ -7,14 +7,12 @@ from sklearn.cluster import KMeans
 from io import BytesIO
 import time, zipfile
 from flasgger import Swagger
-import os
 
 
-
-
+from flask import Flask, request
 app = Flask(__name__)
 swagger = Swagger(app)
-port = int(os.environ.get("PORT", 5000))
+
 
 def cleanse_text(text):
     if text:
